24978a9208cb354b7439f2fe52c54fab45deefd2
ahmad <ahmad@ahmad.com>
2023-07-12 13:45:09 +0330

    fix destroy icon at hamburger menu and convert import css to 'link ref' for better performance

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ let destroy_chk = 0;
 let destroy_text = true; 
 let elem;
 let nElem;
+let elemText;
 try{
   function hamburger() {
       const x = document.getElementById("hamburger");    
@@ -17,6 +18,7 @@ try{
         elem.remove()
         console.log("everything gooooooooone!!!!");        
         nElem.textContent = "You Destroyed body Section.";        
+        elemText.textContent = "You Destroyed body Section.";    
         return;        
       }
       else{
@@ -26,16 +28,22 @@ try{
     elem = document.querySelector("main");
     let elemChild = elem.firstElementChild;        
     elemChild.remove();
-    let elemBtn = document.getElementById('angryBtn');
-    elemBtn.style="color:red;";
+    let elemBtn1 = document.getElementById('angryBtn1');
+    let elemBtn2 = document.getElementById('angryBtn2');
+    elemText = document.getElementById('destroyText');
+    elemBtn1.style="color:red;";
+    elemBtn2.style="color:red;";
+    elemText.style="color:red;";
     if(destroy_text){
       nElem = document.createElement('h4');
-      nElem.className='h4 white';
+      nElem.className='h4';
+      nElem.style="color:red;font-size:48px;";
       nElem.id='destroyMakeIt';
       destroy_text=false;
     }    
     destroy_chk += 1;
     nElem.textContent = `You Destroyed it , ${destroy_chk} times.`; 
+    elemText.textContent = `You Destroyed it , ${destroy_chk} times.`; 
     nParent = document.getElementById('destroySection');  
     nParent.appendChild(nElem) 
   }
