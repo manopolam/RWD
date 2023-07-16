d225cb1eaf8c5361fb874ded1501814a7c1da979
manopolam <manopolam@gmail.com>
2023-07-16 20:19:05 +0330

    fixed board html and css and java page

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,8 +1,8 @@
 "use strict";
 
 let a1 = '1-appendChild() return child object but append() return undefined.\n';
-    a1 +='2-appendChild() return child object but append() return undefined.\n';
-    a1 +='3-appendChild() return child object but append() return undefined.\n';
+    a1 +='2-append() allows you to also append string objects but appendChild() only accepts Node objects.\n';
+    a1 +='3-appendChild() can append several nodes and strings but appendChild() can only append one node.\n';
 let elementChosen;
 function showAnswer(str,element,time){    
     elementChosen = document.getElementById(element);
