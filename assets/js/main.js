16234acf3280fb96c3fbec8d38e91726104de33c
manopolam <manopolam@gmail.com>
2023-07-17 07:33:37 +0330

    fixed hamburger menu because 'use strict' and try catch on main.js have a problem, for that remove try catch to fixed hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,51 +4,47 @@ let destroy_text = true;
 let elem;
 let nElem;
 let elemText;
-try{
-  function hamburger() {
-      const x = document.getElementById("hamburger");    
-      if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
-  }
-  function destroy(){   
-    if(destroy_chk > 2){
-      if(confirm('you want more?')){
-        elem.remove()
-        console.log("everything gooooooooone!!!!");        
-        nElem.textContent = "You Destroyed body Section.";        
-        elemText.textContent = "You Destroyed body Section.";    
-        return;        
-      }
-      else{
-        document.location.reload();
-      }      
-    }    
-    elem = document.querySelector("main");
-    let elemChild = elem.firstElementChild;        
-    elemChild.remove();
-    let elemBtn1 = document.getElementById('angryBtn1');
-    let elemBtn2 = document.getElementById('angryBtn2');
-    elemText = document.getElementById('destroyText');
-    elemBtn1.style="color:red;";
-    elemBtn2.style="color:red;";
-    elemText.style="color:red;";
-    if(destroy_text){
-      nElem = document.createElement('h4');
-      nElem.className='h4';
-      nElem.style="color:red;font-size:48px;";
-      nElem.id='destroyMakeIt';
-      destroy_text=false;
-    }    
-    destroy_chk += 1;
-    nElem.textContent = `You Destroyed it , ${destroy_chk} times.`; 
-    elemText.textContent = `You Destroyed it , ${destroy_chk} times.`; 
-    nParent = document.getElementById('destroySection');  
-    nParent.appendChild(nElem) 
-  }
+
+function hamburger() {
+    const x = document.getElementById("hamburger");    
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
 }
-catch(err){    
-  console.log(err);
+function destroy(){   
+  if(destroy_chk > 2){
+    if(confirm('you want more?')){
+      elem.remove()
+      console.log("everything gooooooooone!!!!");        
+      nElem.textContent = "You Destroyed body Section.";        
+      elemText.textContent = "You Destroyed body Section.";    
+      return;        
+    }
+    else{
+      document.location.reload();
+    }      
+  }    
+  elem = document.querySelector("main");
+  let elemChild = elem.firstElementChild;        
+  elemChild.remove();
+  let elemBtn1 = document.getElementById('angryBtn1');
+  let elemBtn2 = document.getElementById('angryBtn2');
+  elemText = document.getElementById('destroyText');
+  elemBtn1.style="color:red;";
+  elemBtn2.style="color:red;";
+  elemText.style="color:red;";
+  if(destroy_text){
+    nElem = document.createElement('h4');
+    nElem.className='h4';
+    nElem.style="color:red;font-size:48px;";
+    nElem.id='destroyMakeIt';
+    destroy_text=false;
+  }    
+  destroy_chk += 1;
+  nElem.textContent = `You Destroyed it , ${destroy_chk} times.`; 
+  elemText.textContent = `You Destroyed it , ${destroy_chk} times.`; 
+  nParent = document.getElementById('destroySection');  
+  nParent.appendChild(nElem) 
 }
