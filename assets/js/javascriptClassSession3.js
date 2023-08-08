c83ba7e70925d4d1fabd0609be740786d6ffa937
manopolam <manopolam@gmail.com>
2023-08-08 08:24:13 +0330

    implement array.reverse() with 2 solution

--- assets/js/javascriptClassSession3.js
+++ assets/js/javascriptClassSession3.js
@@ -11,9 +11,7 @@ const array2D = [[1,10],
                 [2,20],
                 [3,30]];
 
-// use let instead const to implement reverse method of array and modify source array
-let array10 = [2,5,232,432,76,232,56,232,98,231];
-
+const array10 = [2,5,232,432,76,232,56,232,98,231];
 
 // ---------------------------------------Functions Declaration------------------------------------------
 //Takes a number as input and returns the number of digits.
@@ -71,12 +69,22 @@ function biggestNumberArray(array){
 }
 
 //Implement the reverse method of array
-function reverseArray(array){
+function reverseArray1(array){
     let reverse = [];
     for(let i = array.length-1 ; i >= 0 ; i--){
         reverse.push(array[i]);
     }    
     return reverse;
+}
+
+//Implement the reverse method of array, exactly like array.reverse()
+function reverseArray2(array){
+    const arrayLenght = array.length;
+    for(let i = arrayLenght-1 ; i >= 0 ; i--){
+        array.push(array[i]); //push new item
+        array.splice(i,1); //remove old item
+    }
+    return array;
 }
 
 //an array of numbers and a number as inputs, and then
@@ -91,48 +99,53 @@ function removeRepetitionArray(array,repetition){
 
 // ---------------------------------------Result------------------------------------------
 
-console.groupCollapsed("Digits");
-console.log("The number value is = " + number);
+// console.groupCollapsed("Digits");
+// console.log("The number value is = " + number);
 
-console.log("Count = " + countingDigits(number));
+// console.log("Count = " + countingDigits(number));
 
-console.log("Sum = " + sumDigits(number));
-console.groupEnd("Digits");
+// console.log("Sum = " + sumDigits(number));
+// console.groupEnd("Digits");
 
-console.groupCollapsed("multiplies");
-console.log("The input Object is = ");
-console.log(obj);
-console.log("Result after execute the multiplyNumeric(obj) function = ");
-console.log(multiplyNumeric(obj));
-console.groupEnd("multiplies");
+// console.groupCollapsed("multiplies");
+// console.log("The input Object is = ");
+// console.log(obj);
+// console.log("Result after execute the multiplyNumeric(obj) function = ");
+// console.log(multiplyNumeric(obj));
+// console.groupEnd("multiplies");
 
-console.groupCollapsed("Show a 2D Array");
-console.log(array2D);
-printArray(array2D);
-console.groupEnd("Show a 2D Array");
+// console.groupCollapsed("Show a 2D Array");
+// console.log(array2D);
+// printArray(array2D);
+// console.groupEnd("Show a 2D Array");
 
-//Calculate the biggest number from 4 ways
-console.groupCollapsed("Biggest");
-console.warn("Solution 1 : ascending")
-array10.sort((a,b) => b-a);
-console.log(array10[0]);
-console.warn("Solution 2 : function")
-console.log(`The Biggest Number of array is = ${biggestNumberArray(array10)}`);
-console.warn("Solution 3 : descending")
-array10.sort((a,b) => a-b);
-console.log(array10[array10.length-1]);
-console.warn("Solution 4 : Math.max.apply")
-console.log(Math.max.apply(null,array10));
-console.groupEnd("Biggest");
+// //Calculate the biggest number from 4 ways
+// console.groupCollapsed("Biggest");
+// console.warn("Solution 1 : ascending")
+// array10.sort((a,b) => b-a);
+// console.log(array10[0]);
+// console.warn("Solution 2 : function")
+// console.log(`The Biggest Number of array is = ${biggestNumberArray(array10)}`);
+// console.warn("Solution 3 : descending")
+// array10.sort((a,b) => a-b);
+// console.log(array10[array10.length-1]);
+// console.warn("Solution 4 : Math.max.apply")
+// console.log(Math.max.apply(null,array10));
+// console.groupEnd("Biggest");
 
 console.groupCollapsed("Reverse");
+console.warn("Solution 1 : without change source Array")
 console.log(array10);
-array10 = reverseArray(array10);
+console.log(reverseArray1(array10));
+console.log(array10);
+console.warn("Solution 2 : exactly like array.reverse()")
+console.log(array10);
+reverseArray2(array10);
 console.log(array10);
 console.groupEnd("Reverse");
 
-console.groupCollapsed("Repetition");
-console.log(array10);
-removeRepetitionArray(array10,232);
-console.log(array10);
-console.groupEnd("Repetition");
+// console.groupCollapsed("Repetition");
+// console.log(array10);
+// removeRepetitionArray(array10,232);
+// console.log(array10);
+// console.groupEnd("Repetition");
