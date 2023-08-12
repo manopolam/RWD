8f28db94b1fcad7b79a5fdaafa49a6fedf1f9b01
manopolam <manopolam@gmail.com>
2023-08-12 17:10:56 +0330

    make dynamic menu by menu.js

---
+++ assets/js/menu.js
@@ -0,0 +1,36 @@
+const menuItem = [
+  {id:"web" , href:"index.html" , text:"Home"},
+  {id:"web" , href:"question.html" , text:"Question"},
+  {id:"web" , href:"board.html" , text:"Board"},
+  {id:"jsClass" , href:"devTools.html" , text:"DevTools"},
+  {id:"jsClass" , href:"javascriptClass.html" , text:"jsClass"},  
+]
+
+function menu(menuItem,id){  
+  let menuSelect = menuItem.filter(item => {
+            if(item.id === id) return item;
+  });
+  let menuParent = document.getElementById("menu");    
+  for (let index in menuSelect){    
+    const menuLi = document.createElement('li');
+    const menuA = document.createElement('a');
+    menuA.className='link';
+    menuA.setAttribute("href",menuSelect[index].href);    
+    menuA.textContent = menuSelect[index].text;    
+    menuLi.appendChild(menuA);
+    menuParent.appendChild(menuLi);
+  }
+  menuParent = document.getElementById("hamburger");    
+  for (let index in menuSelect){    
+    const menuLi = document.createElement('li');
+    const menuA = document.createElement('a');
+    menuA.className='mobile';
+    menuA.setAttribute("href",menuSelect[index].href);    
+    menuA.textContent = menuSelect[index].text;    
+    menuLi.appendChild(menuA);
+    menuParent.appendChild(menuLi);
+  }  
+}
+
+// menu(menuItem,"web");
+menu(menuItem,"jsClass");
